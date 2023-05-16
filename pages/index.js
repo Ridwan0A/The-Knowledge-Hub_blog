29253afd184ca1b827acfe5381e6c45d765e121f
Ridwan <ridwan0abdullahi@gmail.com>
2023-05-16 14:11:22 -0500

    created first post

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import Head from 'next/head';
 
-const HomePage = () => {
-  return <div>Welcome to the Knowledge Hub Blog!</div>;
-};
+const posts = [
+  { title: 'Mastering the Art of Web Development: A Comprehensive Guide', excerpt: ' Welcome to Web-Dev' },
+  { title: 'What is React?', excerpt: ' React?' },
+];
 
-export default HomePage;
+export default function Home() {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <Head>
+        <title>The Knowledge Hub</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="grid grid-cols-1 lg:grid-cols-12 gap-12">
+        <div className='lg:col-span-8 col-span-1'>
+        {posts.map((post, index) => (
+          <div key={index}>
+            <h2>{post.title}</h2>
+            <p>{post.excerpt}</p>
+          </div>
+        ))}
+        </div>
+        <div className='lg:col-span-4 col-span-1'>
+            <div className='lg:sticky relative top-8'>
+
+            </div>
+        </div>
+      </div>
+    </div>
+  );
+}
